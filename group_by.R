7dfc2520ceee918eb70717ceb816bee71c2fc2b5
Arup Bhunia <arupsom25@gmail.com>
2018-12-04 19:44:51 +0530

    adding rank function example inside the pgm
    Group_by() takes an existing tbl and converts it into a grouped tbl where operations are performed "by group".
     it’s related to concept of “split-apply-combine”.
     rank() takes a group of values and calculates the rank of each value within the group.

--- group_by.R
+++ group_by.R
@@ -3,3 +3,16 @@ g=hflights %>%
   summarise(p_canc = mean(Cancelled == 1)*100,
            avg_delay = mean(ArrDelay,na.rm = TRUE))%>%
   arrange(avg_delay, p_canc)
+#####rank function#######################
+
+hflights %>%
+
+  filter(!is.na(ArrDelay), ArrDelay>0) %>%
+
+  group_by(UniqueCarrier) %>%
+
+  summarise (avg = mean(ArrDelay))%>%
+
+  mutate(rank = rank(avg)) %>%
+
+  arrange(rank)
